d71d7afa25f06457c3ef3adc5a6f3510143c4d08
yunjunho <yunjunhojj@gmail.com>
2026-01-19 02:46:13 +0900

    feat: UNAUTHORIZED 에러 발생시 로그인 페이지로 이동

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { Configuration } from "./api";
 import { AuthAPIApi, ExchangeRateAPIApi, OrderAPIApi, WalletAPIApi } from "./api";
-import { getAuthToken } from "./auth";
+import { getAuthToken, removeAuthToken } from "./auth";
 
 const apiConfig = new Configuration({
     basePath: "/api",
@@ -9,7 +10,20 @@ const apiConfig = new Configuration({
     },
 });
 
-export const authApi = new AuthAPIApi(apiConfig);
-export const exchangeRateApi = new ExchangeRateAPIApi(apiConfig);
-export const orderApi = new OrderAPIApi(apiConfig);
-export const walletApi = new WalletAPIApi(apiConfig);
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && (error.response.status === 401 || error.response.data?.code === "UNAUTHORIZED")) {
+            removeAuthToken();
+            window.location.href = "/";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export const authApi = new AuthAPIApi(apiConfig, undefined, axiosInstance);
+export const exchangeRateApi = new ExchangeRateAPIApi(apiConfig, undefined, axiosInstance);
+export const orderApi = new OrderAPIApi(apiConfig, undefined, axiosInstance);
+export const walletApi = new WalletAPIApi(apiConfig, undefined, axiosInstance);
